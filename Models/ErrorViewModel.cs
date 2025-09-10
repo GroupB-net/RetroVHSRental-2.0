f3dd3bb39823789b262bc56eac146cd4a19c1095
Andreas Ekman <adde930505@hotmail.com>
2025-09-10 10:28:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetroVHSRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
