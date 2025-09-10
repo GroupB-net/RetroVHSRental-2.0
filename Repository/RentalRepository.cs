94c0304baedf9c298e8112e481a259d331532946
Andreas Ekman <adde930505@hotmail.com>
2025-09-10 11:39:27 +0200

    Add rental repository interface and implementation
    Introduces the `IRentalRepository` interface for rental operations, including methods for retrieving, adding, updating, and removing rentals. Implements this interface in `RentalRepository.cs`, utilizing `ApplicationDbContext` for asynchronous database interactions. This change improves the structure and organization of rental management functionality.

---
+++ Repository/RentalRepository.cs
@@ -0,0 +1,40 @@
+﻿using RetroVHSRental.Models;
+using RetroVHSRental.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetroVHSRental.Repository
+{
+    public class RentalRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public RentalRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Rental>> GetAllAsync()
+        {
+            return await context.Rentals.ToListAsync();
+        }
+        public async Task<Rental> GetByIdAsync(int id)
+        {
+            return await context.Rentals.FirstOrDefaultAsync(b => b.RentalId == id);
+        }
+        public async Task AddAsync(Rental rental)
+        {
+            context.Rentals.Add(rental);
+            await context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Rental rental)
+        {
+            context.Rentals.Update(rental);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(Rental rental)
+        {
+            context.Rentals.Remove(rental);
+            await context.SaveChangesAsync();
+        }
+    }
+}
