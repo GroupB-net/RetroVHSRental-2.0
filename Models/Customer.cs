e76ff5d922733af31b90e5d453bedf76de577ecc
Andreas Ekman <adde930505@hotmail.com>
2025-09-10 11:18:35 +0200

    Add Customer class with properties and data annotations
    Introduces a new `Customer` class in the `RetroVHSRental.Models` namespace.
    The class includes properties for customer details such as `CustomerId`, `StoreId`,
    `FirstName`, `LastName`, `Email`, `AddressId`, `Active`, `CreateDate`, and
    `LastUpdate`. Data annotations are added for database column mappings and to
    designate the primary key for `CustomerId`.

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetroVHSRental.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Column("customer_id")]
+        public int CustomerId { get; set; }
+        [Column("store_id")]
+        public int StoreId { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Column("address_id")]
+        public int AddressId { get; set; }
+        public bool Active { get; set; }
+        [Column("create_date")]
+        public DateTime CreateDate { get; set; }
+        [Column("last_update")]
+        public DateTime LastUpdate { get; set; }
+    }
+}
