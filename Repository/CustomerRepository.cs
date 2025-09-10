b95c0e4dff3f4b6f0b38493580a6f176a6246fe3
Andreas Ekman <adde930505@hotmail.com>
2025-09-10 11:28:04 +0200

    Refactor customer repository and update project file
    Updated `RetroVHSRental.csproj` to change package dependency management and removed the "Repository" folder reference. Added `CustomerRepository` class implementing `ICustomerRepository` with methods for retrieving customers asynchronously. Introduced `ICustomerRepository` interface to define the contract for customer data access.

---
+++ Repository/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using RetroVHSRental.Models;
+using RetroVHSRental.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetroVHSRental.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetByIdAsync(int id)
+        {
+            return await context.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+        }
+    }
+}
