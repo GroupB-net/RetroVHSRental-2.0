5ecb2a5ca06a4cc60c2c700298994cfd6db7d8d9
lienglen <li.englen98@gmail.com>
2025-09-10 11:18:13 +0200

    Created Film Model with properties

---
+++ Models/Film.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RetroVHSRental.Models
+{
+
+    public class Film
+    {
+        [Key]
+        [Column("film_id")]
+        public int FilmId { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        [Column("release_year")]
+        public string Release_year { get; set; }
+        [Column("rental_duration")]
+        public int Rental_duration {  get; set; }
+        [Column("rental_rate")]
+        public decimal Rental_rate { get; set; }
+
+    }
+}
